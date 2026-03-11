6be39ca85d9e62e90121961d91ebd369b00d54d2
solivalle <127651078+solivalle@users.noreply.github.com>
2026-03-10 21:03:23 -0600

    docker-compose fixes

--- gestor/settings.py
+++ gestor/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "NAME":"tienda",
         "USER":"postgres",
         "PASSWORD":"my_pass",
-        "HOST":"127.0.0.1",
+        "HOST":"db",
         "PORT":"5432",
     }
 }
